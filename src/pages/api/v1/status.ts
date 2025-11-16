f91a36ba78e8181f2af3914173e5de831a9d8b29
Lucas Galvão Janot <113521855+lucasgjanot@users.noreply.github.com>
2025-11-16 23:34:16 +0000

    finish

--- src/pages/api/v1/status.ts
+++ src/pages/api/v1/status.ts
@@ -1,13 +1,54 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import database from "infra/database";
+const cfg = require("config.ts");
 
 export type statusResponse = {
-  status: string;
+  updated_at: string;
+  dependencies: {
+    database: {
+      version: string;
+      max_connections: number | "Error";
+      opened_connections: number | "Error";
+    };
+  };
 };
 
 async function statusHandler(_: NextApiRequest, res: NextApiResponse) {
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  let databaseVersionValue = "Error";
+  if (databaseVersionResult) {
+    databaseVersionValue = databaseVersionResult.rows[0].server_version;
+  }
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  let databaseMaxConnectionsValue = "Error";
+  if (databaseMaxConnectionsResult) {
+    databaseMaxConnectionsValue =
+      databaseMaxConnectionsResult.rows[0].max_connections;
+  }
+
+  const databaseName = cfg.db.database;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*) FROM pg_stat_activity WHERE datname=$1",
+    values: [databaseName],
+  });
+  let databaseOpenedConnectionsValue = "Error";
+  if (databaseOpenedConnectionsResult) {
+    databaseOpenedConnectionsValue =
+      databaseOpenedConnectionsResult.rows[0].count;
+  }
+
   res.status(200).json({
-    status: "OK",
+    updated_at: new Date().toISOString(),
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: parseInt(databaseOpenedConnectionsValue),
+      },
+    },
   } satisfies statusResponse);
 }
 
