0aef11935b89f49fa67a11c14d878391f0160c6f
Lucas Galvão Janot <113521855+lucasgjanot@users.noreply.github.com>
2025-11-16 10:34:44 +0000

    fix return of /api/v1/status

--- src/pages/api/v1/status.ts
+++ src/pages/api/v1/status.ts
@@ -2,16 +2,12 @@ import { NextApiRequest, NextApiResponse } from "next";
 import database from "../../../infra/database";
 
 export type statusResponse = {
-  APIstatus: string;
-  DBstatus: string;
+  status: string;
 };
 
 async function statusHandler(_: NextApiRequest, res: NextApiResponse) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  const isConnecting = await database.testConnection();
   res.status(200).json({
-    APIstatus: "OK",
-    DBstatus: isConnecting ? "OK" : "Failed",
+    status: "OK",
   } satisfies statusResponse);
 }
 
