6cf1105374a7146ef97b34116151be5c79a94340
Lucas Galvão Janot <113521855+lucasgjanot@users.noreply.github.com>
2025-11-20 04:19:04 +0000

    fix tests

--- src/tests/integration/api/v1/status/get.test.ts
+++ src/tests/integration/api/v1/status/get.test.ts
@@ -1,8 +1,8 @@
 describe("API GET TESTS", () => {
   test("GET on /api/v1/status should return 200 and OK JSON", async () => {
     const response = await fetch("http://localhost:3000/api/v1/status");
-    const responseBody = await response.json();
     expect(response.status).toBe(200);
+    const responseBody = await response.json();
 
     const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
     expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
