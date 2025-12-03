7993bff6105107228de765894d4194b385e7191f
Lucas Galvão Janot <113521855+lucasgjanot@users.noreply.github.com>
2025-12-03 00:21:39 +0000

    refactor: change `/status` page layout

--- src/pages/status/index.tsx
+++ src/pages/status/index.tsx
@@ -3,55 +3,67 @@ import useSWR from "swr";
 
 async function fetchAPI(key: string) {
   const response = await fetch(key);
-  const responseBody = await response.json();
-  return responseBody;
+  if (!response.ok) throw new Error(`Erro ao carregar ${key}`);
+  return response.json();
 }
 
 export default function SutatusPage() {
   return (
     <>
       <h1>Status</h1>
-      <StatusInfo />
+      <UpdatedAt />
+      <h1>Database</h1>
+      <DatabaseStatus />
     </>
   );
 }
 
-function StatusInfo() {
-  const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
-    refreshInterval: 2000,
-  });
+function UpdatedAt() {
+  const { isLoading, data } = useSWR<StatusResponse>(
+    "/api/v1/status",
+    fetchAPI,
+    {
+      refreshInterval: 2000,
+    },
+  );
   let UpdatedAtText = "Loading...";
-  let DatabaseVersionText = "Loading...";
-  let MaxConnectionsText: string | "Error" | number = "Loading...";
-  let OpenedConnectionsText: string | "Error" | number = "Loading...";
 
   if (!isLoading && data) {
-    const statusBody: StatusResponse = data;
-    UpdatedAtText = new Date(statusBody.updated_at).toLocaleString("us");
-    DatabaseVersionText = statusBody.dependencies.database.version;
-    MaxConnectionsText = statusBody.dependencies.database.max_connections;
-    OpenedConnectionsText = statusBody.dependencies.database.opened_connections;
+    UpdatedAtText = new Date(data.updated_at).toLocaleString("us");
   }
 
   return (
-    <ul>
-      <li>
-        <b>Last Update: </b> {UpdatedAtText}
-      </li>
-      <li>
-        <b>Database:</b>
-        <ul>
-          <li>
-            <b>Version: </b> {DatabaseVersionText}
-          </li>
-          <li>
-            <b>Max Connections: </b> {MaxConnectionsText}
-          </li>
-          <li>
-            <b>Opened Connections: </b> {OpenedConnectionsText}
-          </li>
-        </ul>
-      </li>
-    </ul>
+    <>
+      <b>Last Update: </b>
+      {UpdatedAtText}
+    </>
   );
+}
+
+function DatabaseStatus() {
+  const { isLoading, data } = useSWR<StatusResponse>(
+    "/api/v1/status",
+    fetchAPI,
+    {
+      refreshInterval: 2000,
+    },
+  );
+  let DatabaseStatusText = <div>Loading...</div>;
+  if (!isLoading && data) {
+    DatabaseStatusText = (
+      <>
+        <div>
+          <b>Version: </b> {data.dependencies.database.version}
+        </div>
+        <div>
+          <b>Max Connections: </b> {data.dependencies.database.max_connections}
+        </div>
+        <div>
+          <b>Opened Connections: </b>{" "}
+          {data.dependencies.database.opened_connections}
+        </div>
+      </>
+    );
+  }
+  return DatabaseStatusText;
 }
