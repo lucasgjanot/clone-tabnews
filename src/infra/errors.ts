0bfef0b922216ce4e3cd235f0a57c6013d3aaf9d
Lucas Galvão Janot <113521855+lucasgjanot@users.noreply.github.com>
2025-12-05 04:44:37 +0000

    feat: feat: add new custom errors and refactor error handling

--- src/infra/errors.ts
+++ src/infra/errors.ts
@@ -1,11 +1,33 @@
-export class InternalServerError extends Error {
+import { NextApiRequest, NextApiResponse } from "next";
+
+export type ErrorResponse = {
+  name: string;
+  message: string;
+  action: string;
+  status_code: number;
+};
+
+class BaseHttpError extends Error {
   action: string;
   statusCode: number;
-  constructor({ cause }: { cause?: Error }) {
-    super("unexpected error", { cause });
-    this.name = "InternalServerError";
-    this.action = "contact support for help";
-    this.statusCode = 500;
+
+  constructor({
+    message,
+    action,
+    statusCode,
+    name,
+    cause,
+  }: {
+    name: string;
+    message: string;
+    action: string;
+    statusCode: number;
+    cause?: Error;
+  }) {
+    super(message, { cause });
+    this.name = name;
+    this.action = action;
+    this.statusCode = statusCode;
   }
 
   toJSON() {
@@ -16,4 +38,46 @@ export class InternalServerError extends Error {
       status_code: this.statusCode,
     };
   }
+}
+
+export class InternalServerError extends BaseHttpError {
+  constructor({ cause }: { cause?: Error }) {
+    super({
+      name: "InternalServerError",
+      message: "Unexpected Error",
+      action: "Contact support for help",
+      statusCode: 500,
+      cause: cause,
+    });
+  }
+}
+
+export class MethodNotAllowedError extends BaseHttpError {
+  constructor({ method }: { method: string }) {
+    super({
+      name: "MethodNotAllowedError",
+      message: `'${method}' method not allowed in this endpoint`,
+      action: "Verify if sent HTTP method is allowed in this endpoint",
+      statusCode: 405,
+    });
+  }
+}
+
+export function errorHandler(
+  err: unknown,
+  _: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const internalServerError = new InternalServerError({ cause: err as Error });
+  console.error(internalServerError);
+  res.status(internalServerError.statusCode).json(internalServerError.toJSON());
+}
+
+export function notAllowedHandler(req: NextApiRequest, res: NextApiResponse) {
+  const methodNotAllowedError = new MethodNotAllowedError({
+    method: req.method as string,
+  });
+  res
+    .status(methodNotAllowedError.statusCode)
+    .json(methodNotAllowedError.toJSON());
 }
